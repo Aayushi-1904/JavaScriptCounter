55847e4b6da4047ec40a535796c4904508bef7b8
Aayushi Srivastava <80635048+Aayushi-1904@users.noreply.github.com>
2022-09-07 16:04:55 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,21 @@
+function increment()
+{
+    var number= Number(document.getElementById("number").innerHTML);
+
+    document.getElementById("number").innerHTML=number+1;
+
+}
+function decrement()
+{
+    var number= Number(document.getElementById("number").innerHTML);
+
+    document.getElementById("number").innerHTML=number-1;
+
+}
+function neutral()
+{
+    var number= Number(document.getElementById("number").innerHTML);
+
+    document.getElementById("number").innerHTML=0;
+
+}
